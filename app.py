d5e9fca7f59ce70b5dd676f9b686d368f97f59d0
Clément <clement.durand@epitech.eu>
2022-09-12 01:12:07 +0200

    too fix the root with the ai script

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
-
+from toto import main
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return render_template("./index.html", text="This text goes inside the HTML")
-
+    response = main()
+    print("response")
+    if(response != ""):
+        print("its good")
+    return render_template("./index.html", text="Hi there !!")
+    
 @app.route('/start')
 def hello2():
     return render_template("./index2.html", text="This text goes inside the HTML")
